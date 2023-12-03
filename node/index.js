27b4b6eb23d2ca10666c7489064cd720aaad9f82
andrazmen <and.menegatti@gmail.com>
2023-12-03 18:20:40 +0100

    Update

--- node/index.js
+++ node/index.js
@@ -10,10 +10,10 @@ const sockserver = new WebSocketServer({ port: 443 });
 
 sockserver.on("connection", (ws) => {
   console.log("New client connected!");
-  ws.send("connection established");
+  //ws.send("connection established");
 
   ws.on("close", () => console.log("Client has disconnected!"));
-
+  /*
   ws.on("message", (data) => {
     sockserver.clients.forEach((client) => {
       console.log(`distributing message: ${data}`);
@@ -21,6 +21,22 @@ sockserver.on("connection", (ws) => {
       //client.send(JSON.parse(data));
     });
   });
+  */
+  let sender;
+
+  ws.on("message", (data) => {
+    // Save the sender WebSocket connection
+    sender = ws;
+
+    // Distribute the message to all clients except the sender
+    sockserver.clients.forEach((client) => {
+      if (client !== sender) {
+        console.log(`Distributing message: ${data}`);
+        client.send(`${data}`);
+      }
+    });
+  });
+
   ws.onerror = function () {
     console.log("websocket error");
   };
